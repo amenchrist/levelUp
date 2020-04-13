ad6eb6e6108a1f2138dee4ffc5f3f69d2477f5f9
Amenchrist <amenphronesis@gmail.com>
2020-04-13 21:00:55 +0100

    changed component function to class

--- src/App.js
+++ src/App.js
@@ -3,30 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 import { response } from 'express';
 
-componentDidMount() {
-  fetch('https://secret-citadel-16777.herokuapp.com/')
-  .then(res => res.json())
-  .then(console.log);
-}
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.component() {
+
+  componentDidMount() {
+    fetch('https://secret-citadel-16777.herokuapp.com')
+    .then(res => res.json())
+    .then(console.log);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
+  };
 }
 
 export default App;
