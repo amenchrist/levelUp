378a18736275ae401c5f4c55c9daa7a52e72adec
Amenchrist <amenphronesis@gmail.com>
2020-04-13 05:14:48 +0100

    Updates

---
+++ precache-manifest.58b121a118db1b98bb39be44971c21eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a0e264ac1e3ba0fded022f66933993",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44a368049b22270a525a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "db602543bc93d0dea542",
+    "url": "/static/js/2.4654e5f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4654e5f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44a368049b22270a525a",
+    "url": "/static/js/main.7d91e245.chunk.js"
+  },
+  {
+    "revision": "37bf182b9de53a18a16e",
+    "url": "/static/js/runtime-main.cf8d623d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
