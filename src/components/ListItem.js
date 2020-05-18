9d9717d9bb77538f5aec52e73264ff8aeb10bccb
Amenchrist <amenphronesis@gmail.com>
2020-05-18 12:14:28 +0100

    minor css tweak to list items

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -7,7 +7,7 @@ export default function ListItem( { touchFunction, item }){
     switch(item.type){
         case PROJECT:
             return (
-                <div className='ba pa2 ma2 listItem w-100 flex justify-between' id={item.id} onClick={touchFunction}>
+                <div className='ba pa2 ma2 listItem w-100 flex justify-between h-20 items-center' id={item.id} onClick={touchFunction}>
                     <div className='w-80'>
                     <p className='fw7 b white pb2'>{item.name}</p>
                     <p className='fw3 white'>Due: {(new Date(item.dueDate)).toISOString().substr(0, 10)}. Tasks: {item.taskList.length}</p>
@@ -19,7 +19,7 @@ export default function ListItem( { touchFunction, item }){
             )
         case TASK:
             return (
-                <div className='ba pa2 ma2 listItem w-100 flex justify-between' id={item.id} onClick={touchFunction}>
+                <div className='ba pa2 ma2 listItem w-100 flex justify-between h-20 items-center' id={item.id} onClick={touchFunction}>
                     <div className='w-80'>
                     <p className='fw7 b white pb2'>{item.name}</p>
                     <p className='fw3 white'>{item.requiredContext}</p>
@@ -31,7 +31,7 @@ export default function ListItem( { touchFunction, item }){
             )
         case INBOX_ITEM:
             return (
-                <div className='ba pa2 ma2 listItem w-100 flex justify-between' id={item.id} onClick={touchFunction}>
+                <div className='ba pa2 ma2 listItem w-100 flex justify-between h-20 items-center' id={item.id} onClick={touchFunction}>
                     <div className='w-80'>
                     <p className='fw7 b white pb2'>{item.name}</p>
                     <p className='fw3 white'>{(new Date(item.entryDate)).toISOString().substr(0, 10)}</p>
